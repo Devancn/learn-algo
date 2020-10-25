6722e00888c764ff62522ea860e43616c905c2d7
dingxiaohuan <wydingxiaohuan@163.com>
2020-10-25 15:21:11 +0800

    bubble sort

---
+++ srot.js
@@ -0,0 +1,18 @@
+/**
+ * 冒泡排序
+ * @param {array} arr 
+ */
+function bubbleSort(arr) {
+  if (arr.length <= 1) return;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+console.log(bubbleSort([2, 1, 0, 3, 5, 4]));
