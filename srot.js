5bcbd00ace99f3ceac09dab7b80e02965db19e57
dingxiaohuan <wydingxiaohuan@163.com>
2020-10-25 16:20:46 +0800

    selection sort

--- srot.js
+++ srot.js
@@ -15,18 +15,18 @@ function bubbleSort(arr) {
   }
   return arr;
 }
-console.log(bubbleSort([2, 1, 0, 3, 5, 4]));
+// console.log(bubbleSort([2, 1, 0, 3, 5, 4]));
 
 /**
  * 插入排序
- * @param {array} arr 
+ * @param {array} arr
  */
 function insertion(arr) {
   if (arr.length <= 1) return;
   for (let i = 1; i < arr.length; i++) {
     const temp = arr[i];
     let j = i - 1;
-    for (j; j >= 0; j++) {
+    for (j; j >= 0; j--) {
       if (arr[j] > temp) {
         arr[j + 1] = arr[j];
       } else {
@@ -37,4 +37,25 @@ function insertion(arr) {
   }
   return arr;
 }
-console.log(insertion([2, 1, 0, 3, 5, 4]));
+// console.log(insertion([2, 1, 0, 3, 5, 4]));
+
+/**
+ * 选择排序
+ * @param {array} arr
+ */
+function selectionSort(arr) {
+  if (arr.length <= 1) return;
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    const temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
+  }
+  return arr;
+}
+console.log(selectionSort([2, 1, 0, 3, 5, 4]));
