7c5be2cc2198657c76c1984f64d026c650597d5d
dingxiaohuan <wydingxiaohuan@163.com>
2020-11-04 19:09:57 +0800

    二分查找第一个与target的值相等bug修复

--- binary-find3.js
+++ binary-find3.js
@@ -1,9 +1,11 @@
 /**
  * 在array中查找第一个与target相同的值
+ * arry必须是有序数组
  * @param {array} array
  * @param {number} target
  */
 function binaryFindFirst(array = [], target) {
+  if(array.length === 0) return -1;
   let start = 0;
   let end = array.length - 1;
   while (start < end) {
@@ -15,9 +17,9 @@ function binaryFindFirst(array = [], target) {
     } else {
       /*
         1. 如果mid等于0，那这个元素已经是数组的第一个元素
-        2. a[mid]的前一个元素a[m-1]不等于value，则说明a[mid]就是要找到的元素
+        2. a[mid]的前一个元素a[m-1]小于value，则说明a[mid]就是要找到的元素
       */
-      if(mid === 0 || array[mid - 1] !== target) {
+      if(mid === 0 || array[mid - 1] < target) {
         return mid
       } else {
         end = mid - 1;
@@ -25,5 +27,6 @@ function binaryFindFirst(array = [], target) {
     }
   }
 }
-const array = [1, 2, 3, 4, 4, 4, 4, 4, 6, 7, 8, 8, 9];
-console.log(binaryFindFirst(array,4))
+const arr = [4,5,4,5,6]
+
+console.log(binaryFindFirst(arr,4))
