c371860a1c09c0bd9aa2a179030d4c7f39dfc6e3
dingxiaohuan <wydingxiaohuan@163.com>
2020-11-10 20:47:18 +0800

    search tree insert

--- binary_tree.js
+++ binary_tree.js
@@ -1,4 +1,3 @@
-// 树种的每个节点
 class Node {
   constructor(value) {
     this.value = value;
@@ -6,51 +5,90 @@ class Node {
     this.right = null;
   }
 }
-
-// 搜索二叉树
+/**
+ * 搜索二叉树
+ */
 class SearchTree {
   constructor() {
     this.root = null;
   }
+  insert(num) {
+    let node = new Node(num);
+    if (this.root === null) {
+      this.root = node;
+      return;
+    }
+    let parent = this.getPrev(num);
+    if (num < parent.value) {
+      parent.left = node;
+    } else {
+      parent.right = node;
+    }
+  }
+  //中序遍历
+  print() {
+    let point = this.root;
+    if (point) {
+      printAll(point.left);
+      console.log(point.value);
+      printAll(point.right);
+    }
+    function printAll(point) {
+      if (point == null) {
+        return;
+      }
+      printAll(point.left);
+      console.log(point.value);
+      printAll(point.right);
+    }
+  }
+  //添加和查找的公用部分
   getPrev(num, find = false) {
-    const point = this.root;
+    let point = this.root;
     let res = [];
-    while(true && point) {
-      // 从左节点中查找值比num小的节点
-      if(point.left) {
-        if(num < point.left.value || num < point.value) {
+    while (true) {
+      if (point.left) {
+        if (num < point.left.value || num < point.value) {
           point = point.left;
           continue;
         }
       }
 
-      if(point.right) {
-        if(num >= point.right.value || num >= point.value) {
-          // 如果有多个节点值相同则缓存
-          if(find && num === point.value) {
+      if (point.right) {
+        if (num >= point.right.value || num >= point.value) {
+          //搜索时如果有多个值则缓存
+          if (find && num === point.value) {
             res.push(point.value);
           }
           point = point.right;
           continue;
         }
       }
-      
-      if(find) {
-        if(point.value === num) {
+      //如果是搜索
+      if (find) {
+        if (point.value === num) {
           res.push(point.value);
         }
 
-        if(res.length === 0) {
+        if (res.length === 0) {
           return null;
         }
 
-        if(res.length === 1) {
+        if (res.length === 1) {
           return res[0];
         }
 
         return res;
       }
+      //如果是添加 返回的是应该添加的那各节点的父节点
       return point;
     }
   }
 }
+
+let searchTree = new SearchTree();
+searchTree.insert(4);
+searchTree.insert(1);
+searchTree.insert(2);
+searchTree.insert(5);
+searchTree.print();
