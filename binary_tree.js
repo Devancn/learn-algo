d0f20a350daf47645c33cee504bedb4917fdf933
dingxiaohuan <wydingxiaohuan@163.com>
2020-11-11 20:16:00 +0800

    SearchTree remove

--- binary_tree.js
+++ binary_tree.js
@@ -5,6 +5,7 @@ class Node {
     this.right = null;
   }
 }
+
 /**
  * 搜索二叉树
  */
@@ -23,6 +24,82 @@ class SearchTree {
       parent.left = node;
     } else {
       parent.right = node;
+    }
+  }
+  remove(num) {
+    let point = this.root;
+    let parent = null;
+    let tree = this;
+    let res = null;
+
+    while (true) {
+      if (point.left) {
+        if (num < point.left.value || num < point.value) {
+          parent = point;
+          point = point.left;
+          continue;
+        }
+      }
+      if (point.right) {
+        if (num >= point.right.value || num >= point.value) {
+          if (num === point.value) {
+            delMethod(point, parent);
+            if (point === null) {
+              point = this.root;
+            } else {
+              parent = parent;
+              point = parent.right;
+            }
+            res = true;
+            continue;
+          }
+          parent = point;
+          point = point.right;
+          continue;
+        }
+      }
+      if (point.value === null) {
+        res = true;
+        delMethod(point, parent);
+      }
+      break;
+    }
+    return res;
+    function delMethod(delNode, parent) {
+      let p = delNode;
+      let pp = parent;
+
+      // 删除的节点有左右两个子节点
+      if (p.left != null && p.right != null) {
+        // 查找右子树中最小节点
+        let minP = p.right;
+        let minPP = p;
+        // 左边的值小于右边的值
+        while (minP.left != null) {
+          minPP = minP;
+          minP = minP.left;
+        }
+        // 用找到最小节点值赋值给要删除的节点的value
+        p.value = minP.value;
+        p = minP;
+        pp = minPP;
+      }
+
+      let child;
+      if (p.left !== null) {
+        child = p.left;
+      } else if (p.right !== null) {
+        child = p.right;
+      } else {
+        child = null;
+      }
+      if ((pp = null)) {
+        tree.root = child;
+      } else if (pp.left === p) {
+        pp.left = child;
+      } else {
+        pp.right = child;
+      }
     }
   }
   //中序遍历
