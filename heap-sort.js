a56f6857eccc144187514488941072b489f9b159
dingxiaohuan <wydingxiaohuan@163.com>
2020-11-12 20:42:14 +0800

    heap sort

--- heap-sort.js
+++ heap-sort.js
@@ -2,27 +2,28 @@
 class HeapSort{
   constructor(originArray) {
     this.originArray = originArray;
-    console.log(this.originArray);
   }
   buildHeap() {
     // 用数组来存储索引为0的不存
-    for(let i =  this.originArray.length; i >= 1; i--) {
-      this.heapify(i);
+    const arr =  this.originArray;
+    for(let i =  arr.length; i >= 1; i--) {
+      this.heapify(arr, arr.length, i);
     }
+    return arr;
   }
 
   // 自底向上堆化操作
-  heapify(i) {
+  heapify(arr, length,i) {
     while(true) {
       //首先根据当前索引i的元素与其左/右节点中找出最大的一个
       // 堆（完全二叉树）的左节点为索引为 i * 2,右节点索引为 i * 2 + 1
       let maxPos = i;
       // 如果 索引i的左节点2*i存在且比当前索引i的元素大
-      if(i * 2 <= this.originArray.length && arr[i] < arr[i * 2]) {
+      if(i * 2 <= length && arr[i] < arr[i * 2]) {
         maxPos = i * 2;
       }
       // 如果索引i有右节点且比MaxPos节点大
-      if(i * 2 + 1 <= this.originArray.length && this.originArray[maxPos] < this.originArray[i * 2 + 1]) {
+      if(i * 2 + 1 <= length && arr[maxPos] < arr[i * 2 + 1]) {
         maxPos = i * 2 +1;
       }
       // 如果最大节点的索引与i相同则退出循环体
@@ -30,15 +31,39 @@ class HeapSort{
       // 否则交换位置，父节点为maxPos对应的节点
       if(maxPos === i) break;
       // 索引i与 maxPos(左节点索引或右节点索引)对应节点交换位置
-      this.swap(i, maxPos);
+      this.swap(arr, i, maxPos);
       // 交换完后尝试根据maxPos查找更大的值
       i = maxPos;
     }
 
   }
-  swap(i, maxPos) {
+
+  sort() {
+    const arr = this.buildHeap();
+    let length = arr.length - 1;
+    while(length > 1) {
+      // 交换顶元素和最后一位。顶元素永远是最大的。
+      this.swap(arr, 1, length );
+      length--;
+      //剩下的元素重新建堆 直到len === 1 停止
+      this.heapify(arr, length, 1)
+    }
+    console.log(arr);
+  }
+
+  swap(arr, i, maxPos) {
     let temp = arr[i];
     arr[i] = arr[maxPos];
     arr[maxPos] = temp;
   }
 }
+
+const arr = [null]
+let i = 0
+while (i <= 10) {
+    const num = Math.floor(Math.random() * 100)
+    arr.push(num)
+    i++
+}
+const testHeap = new HeapSort(arr)
+testHeap.sort()
