188bff0b5b4e725276907f91cd4135b0ed59180d
dingxiaohuan <wydingxiaohuan@163.com>
2020-11-14 19:56:10 +0800

    ac ACTree

---
+++ ac-automata.js
@@ -0,0 +1,30 @@
+const MAX_LEN = 128;
+
+class ACNode {
+  constructor(data) {
+    this.data = data;
+    this.children = new Array(MAX_LEN);
+    this.isEndingChar = false;
+    this.length = 0;
+    this.fail = null;
+  }
+}
+
+class ACTree {
+  constructor() {
+    this.root = new ACNode('/');
+  }
+
+  insert(text = '') {
+    let node = this.root;
+    for(let char of text) {
+      let index = char.codePointAt(0) + 1;
+      if(!node.children[index]) {
+        node.children[index] = new ACNode(char);
+      }
+      node = node.children[index];
+    }
+    node.isEndingChar = true;
+    node.length = text.length;
+  }
+}
